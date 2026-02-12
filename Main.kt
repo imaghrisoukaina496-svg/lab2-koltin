30089c5cf73c572855dc71b7631251493672c8e1
soukaina <imaghrisoukaina496@gmail.com>
2026-02-12 10:28:47 +0100

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    // ===== Task 1 =====
+    val weekday = "Friday"
+    val hour = 20   // Format 24h (20 = 8 PM)
+
+    println("On $weekday, the opening hours are:")
+
+    when (weekday) {
+        "Monday" -> println("8 AM to 12 PM")
+        "Tuesday", "Wednesday", "Thursday" -> println("8 AM to 6 PM")
+        "Friday" -> println("8 AM to 9 PM")
+        "Saturday" -> println("9 AM to 4 PM")
+        "Sunday" -> println("8 AM to 4 PM")
+        else -> println("Unknown day")
+    }
+
+    // ===== Task 2 =====
+    val isOpen = when (weekday) {
+        "Monday" -> hour in 8..12
+        "Tuesday", "Wednesday", "Thursday" -> hour in 8..18
+        "Friday" -> hour in 8..21
+        "Saturday" -> hour in 9..16
+        "Sunday" -> hour in 8..16
+        else -> false
+    }
+
+    if (isOpen) {
+        println("Little Lemon is open now")
+    } else {
+        println("Little Lemon is closed now")
+    }
+}
